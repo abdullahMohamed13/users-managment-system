47f2ed99d1c27a0a0d0f708ef1537186643ab3ec
abdullahMohamed13 <abdullah.229op@gmail.com>
2026-05-15 08:39:38 +0300

    chore: rename app

--- client/src/config/env.ts
+++ client/src/config/env.ts
@@ -3,7 +3,7 @@ export const ENV = {
   DEV_MODE: "development",
   API_URL: "http://localhost:3007",
   API_TIMEOUT: 10000,
-  APP_NAME: "ُSurvey land",
+  APP_NAME: "Users management system",
   APP_VERSION: "1.0.0",
   APP_ENV: "development",
   ACCESS_TOKEN_KEY: "accessToken",
